9167e6b7b0bf9e52ec604e76040c8aa1f4fe2802
Patrick Coxall <mr.coxall@mths.ca>
2022-06-01 14:37:57 -0400

    update

--- sh_scripts/setup.sh
+++ sh_scripts/setup.sh
@@ -12,17 +12,17 @@ pip3 install cpplint
 echo "This script will setup your SSH key with GitHub"
 echo ""
 
-echo -e "Please enter your full name (ex: Jane Smith): \c "
+echo "Please enter your full name (ex: Jane Smith): \c "
 read  name
-echo -e "Please enter your email address that you used to setup GitHub (ex: mr.coxall@mths.ca): \c "
+echo "Please enter your email address that you used to setup GitHub (ex: mr.coxall@mths.ca): \c "
 read  email
-echo -e "Please enter your GitHub user name (ex: mr-coxall): \c "
+echo "Please enter your GitHub user name (ex: mr-coxall): \c "
 read  user
 
-echo -e ""
-echo -e "You will next be asked 3 questions."
-echo -e "Just press ENTER each time."
-echo -e ""
+echo ""
+echo "You will next be asked 3 questions."
+echo "Just press ENTER each time."
+echo ""
 
 # setup SSH key
 ssh-keygen -t ed25519 -C "$email"
@@ -31,9 +31,17 @@ echo ""
 cat ~/.ssh/id_ed25519.pub
 
 echo ""
-echo "Please copy and paste the above key into your GitHub SSH Keys: \c "
-echo -e "Once done, press any key. \c "
+echo "Please copy and paste the above key into your GitHub SSH Keys."
+echo "Once done, press any key."
 read  any_key
+
+echo ""
+echo "You will be asked if you want to continue."
+echo "Type yes."
+echo ""
+echo "After doing this a new Codespace window will open."
+echo "Close this window and use the new one."
+#read  any_key
 
 ssh -T git@github.com
 git config --global user.email "$email"
